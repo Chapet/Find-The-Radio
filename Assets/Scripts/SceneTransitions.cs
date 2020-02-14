5677a534cce793134eeb498d612d7d7e1eb6bd76
Louis <louis99laurent@gmail.com>
2020-02-14 13:20:43 +0100

    Add MainMenuScene (New Game, Continue + Transitions)
    https://www.youtube.com/watch?v=zc8ac_qUXQY&
    https://www.youtube.com/watch?v=Qd2em_ts5vs

---
+++ Assets/Scripts/SceneTransitions.cs
@@ -0,0 +1,22 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public string sceneName;
+
+    public void NewGame()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    IEnumerator LoadScene()
+    {
+        transitionAnim.SetTrigger("launch");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
