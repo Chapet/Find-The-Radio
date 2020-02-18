7bc3afd8b85954bedbad543352785661a2b58a22
Achille Morenville <achille.morenville@student.uclouvain.be>
2020-02-18 11:34:23 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/StatusBar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusBar : MonoBehaviour
+{
+
+    public Slider slider;
+
+    public void SetValue(int value) {
+        slider.value = value;
+    }
+
+    public void SetMaxValue(int maxValue) {
+        slider.maxValue = maxValue;
+    }
+}
