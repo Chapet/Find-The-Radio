eaa326abb4a160c81d7a96b2e0eccfa1e78687d4
Olivier <olivierlatteur41@gmail.com>
2020-02-24 12:53:58 +0100

    Pop-Up

---
+++ Assets/Scripts/PopUp/PopUpController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class PopUpController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI message;
+    
+    // Start is called before the first frame update
+    public void show(string msg)
+    {
+        this.message.SetText(msg);
+        gameObject.SetActive(true);
+    }
+
+    public void OnClickOK()
+    {
+        gameObject.SetActive(false);
+    }
+}
