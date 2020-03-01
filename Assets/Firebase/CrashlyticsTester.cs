97353bdf4771f053c640e6e05e54935cdac24b88
Olivier <olivierlatteur41@gmail.com>
2020-03-01 16:22:54 +0100

    Crashlytics is working

--- Assets/Firebase/CrashlyticsTester.cs
+++ Assets/Firebase/CrashlyticsTester.cs
@@ -18,8 +18,19 @@ public class CrashlyticsTester : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update()
+    void Update()    
     {
+        
+        /*var message = new AndroidJavaObject("java.lang.String", "This is a test crash, ignore.");
+        var exception = new AndroidJavaObject("java.lang.Exception", message);
+       
+        var looperClass = new AndroidJavaClass("android.os.Looper");
+        var mainLooper = looperClass.CallStatic<AndroidJavaObject>("getMainLooper");
+        var mainThread = mainLooper.Call<AndroidJavaObject>("getThread");
+        var exceptionHandler = mainThread.Call<AndroidJavaObject>("getUncaughtExceptionHandler");
+        exceptionHandler.Call("uncaughtException", mainThread, exception);*/
+        
+        
         // Call the exception-throwing method here so that it's run
         // every frame update
         if (count < 5)
