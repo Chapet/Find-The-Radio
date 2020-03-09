843a51c81b82197ec0b7fb05292425ab83d299cf
Louis <louis99laurent@gmail.com>
2020-03-09 18:55:51 +0100

    Scavenge Events

--- Assets/Scripts/Menu Controllers/ScavengingController.cs
+++ Assets/Scripts/Menu Controllers/ScavengingController.cs
@@ -7,6 +7,7 @@ public class ScavengingController : MonoBehaviour
 {
     public GameController gameController;
     public GameObject BunkerPanel;
+    public InventoryManager inventory;
     //public GameObject backPanel;
     public Color BrightYellow;
     public Color DarkYellow;
@@ -15,8 +16,8 @@ public class ScavengingController : MonoBehaviour
     private float scavengeTime;
     [Tooltip("Contain the object player")]
     public PlayerController player;
-
     public MenuController menuController;
+    public PopupSystem pop;
 
     void Update()
     {
@@ -53,14 +54,42 @@ public class ScavengingController : MonoBehaviour
         // ajout aléatoire d'items, le compte total ne dépassant pas le maximum transportable
         var rand = new System.Random();
         int nbFound = 0;
+        double chance = 0;
+        bool ev = false;
+
         for (int i = 0; i < nbTimeSlice && nbFound < player.maxCarryingSize; i++)
         {
-            double chance  = rand.NextDouble();
-            if (chance > 0.5)
+            chance  = rand.NextDouble(); // 0.0 <= chance < 1.0
+            if (chance > 0.66)
             {
                 nbFound++;
+
+                //chances for the found item, has to be balance when all items in the game
+                chance = rand.NextDouble();
+
+                if (chance > 0.9) //gun
+                {
+                    Debug.Log("pistol added to inventory");
+                }
+                else if (chance > 0.45) // water
+                {
+                    Debug.Log("water added to inventory");
+                }
+                else //food
+                {
+                    Debug.Log("food can added to inventory");
+                }
+
                 Debug.Log("adding Item: " + i);
             }
+
+            if (!ev)
+            {
+                chance = rand.NextDouble();
+                if (chance > 0.95) pop.PopMessage(PopupSystem.Popup.Death);
+                else if (chance > 0.80) pop.PopMessage(PopupSystem.Popup.Bite);
+            }
+           
         } //le principe est la, il faut balance et ajouter les items à l'inventaire
 
         // updating game values
---
+++ Assets/Scripts/PopUp/PopupSystem.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PopupSystem : MonoBehaviour
+{
+    public TMP_Text textPopupMsg;
+    public Button acceptButton;
+    public Button refuseButton;
+    public Button okButton;
+    public GameObject popPanel;
+    public GameObject backPanel;
+    public PlayerController player;
+    public enum Popup {Bite, Death};
+
+    public void PopMessage(Popup popup) 
+    {
+        popPanel.SetActive(true);
+        backPanel.SetActive(true);
+
+        switch (popup) 
+        {
+            case Popup.Bite:
+                textPopupMsg.text = "   You were biten by a zombie!";
+                player.UpdateHealth(-20);
+                //update player effect: biten/bleeding
+                okButton.gameObject.SetActive(true);
+                break;
+            case Popup.Death:
+                textPopupMsg.text = "   You were assaulted by a crowd of zombies, you didn't make it";
+                player.UpdateHealth(-100);
+                okButton.gameObject.SetActive(true);
+                break;
+        }
+    }
+}
