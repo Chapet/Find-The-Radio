7f3f7c171312a1676cddca8193963bdba88da296
hlibioulle <hadrien.libioulle@student.uclouvain.be>
2020-03-15 16:52:45 +0100

    Same as previous commit

--- Assets/Scripts/Player/StatusBarController.cs
+++ Assets/Scripts/Player/StatusBarController.cs
@@ -7,24 +7,33 @@ public class StatusBarController : MonoBehaviour
     [SerializeField] private List<StatusBar> hungerBars = new List<StatusBar>();
     [SerializeField] private List<StatusBar> thirstBars = new List<StatusBar>();
     [SerializeField] private List<StatusBar> energyBars = new List<StatusBar>();
-    [SerializeField] private PlayerController player;
+    private PlayerController player;
+
+    private void Awake()
+    {
+        player = gameObject.GetComponent<PlayerController>();
+    }
 
     public void UpdateStatusBars()
     {
         foreach(StatusBar s in healthBars)
         {
+            s.SetMaxValue(PlayerController.maxHealth);
             s.SetValue(player.currentHealth);
         }
         foreach (StatusBar s in hungerBars)
         {
+            s.SetMaxValue(PlayerController.maxHunger);
             s.SetValue(player.currentHunger);
         }
         foreach (StatusBar s in thirstBars)
         {
+            s.SetMaxValue(PlayerController.maxThirst);
             s.SetValue(player.currentThirst);
         }
         foreach (StatusBar s in energyBars)
         {
+            s.SetMaxValue(PlayerController.maxEnergy);
             s.SetValue(player.currentEnergy);
         }
     }
